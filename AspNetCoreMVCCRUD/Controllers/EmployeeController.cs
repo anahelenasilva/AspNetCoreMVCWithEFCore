be13f6a737f8eaa4471b63ec614db7c94774b103
Ana Helena <anahelenarp@hotmail.com>
2019-03-14 21:35:01 -0300

    add context

--- AspNetCoreMVCCRUD/Controllers/EmployeeController.cs
+++ AspNetCoreMVCCRUD/Controllers/EmployeeController.cs
@@ -24,27 +24,33 @@ namespace AspNetCoreMVCCRUD.Controllers
             return View(await _context.Employees.ToListAsync());
         }
 
-        // GET: Employee/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: Employee/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var employee = await _context.Employees
-                .FirstOrDefaultAsync(m => m.EmployeeId == id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
+        //    var employee = await _context.Employees
+        //        .FirstOrDefaultAsync(m => m.EmployeeId == id);
+        //    if (employee == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(employee);
-        }
+        //    return View(employee);
+        //}
 
         // GET: Employee/Create
-        public IActionResult Create()
+        public IActionResult AddOrEdit(int id = 0)
         {
+            if (id > 0)
+            {
+                var employee = _context.Employees.FirstOrDefault(f => f.EmployeeId == id);
+                return View(employee);
+            }
+
             return View();
         }
 
@@ -53,67 +59,75 @@ namespace AspNetCoreMVCCRUD.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("EmployeeId,FullName,Code,Position,OfficeLocation")] Employee employee)
+        public async Task<IActionResult> AddOrEdit([Bind("EmployeeId,FullName,Code,Position,OfficeLocation")] Employee employee)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(employee);
+                if (employee.EmployeeId > 0)
+                {
+                    _context.Update(employee);
+                }
+                else
+                {
+                    _context.Add(employee);
+                }
+                
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(employee);
         }
 
-        // GET: Employee/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: Employee/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var employee = await _context.Employees.FindAsync(id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-            return View(employee);
-        }
+        //    var employee = await _context.Employees.FindAsync(id);
+        //    if (employee == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(employee);
+        //}
 
-        // POST: Employee/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,FullName,Code,Position,OfficeLocation")] Employee employee)
-        {
-            if (id != employee.EmployeeId)
-            {
-                return NotFound();
-            }
+        //// POST: Employee/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,FullName,Code,Position,OfficeLocation")] Employee employee)
+        //{
+        //    if (id != employee.EmployeeId)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(employee);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EmployeeExists(employee.EmployeeId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employee);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(employee);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!EmployeeExists(employee.EmployeeId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(employee);
+        //}
 
         // GET: Employee/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -133,16 +147,16 @@ namespace AspNetCoreMVCCRUD.Controllers
             return View(employee);
         }
 
-        // POST: Employee/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var employee = await _context.Employees.FindAsync(id);
-            _context.Employees.Remove(employee);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //// POST: Employee/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var employee = await _context.Employees.FindAsync(id);
+        //    _context.Employees.Remove(employee);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool EmployeeExists(int id)
         {
--- AspNetCoreMVCCRUD/Models/Employee.cs
+++ AspNetCoreMVCCRUD/Models/Employee.cs
@@ -8,16 +8,20 @@ namespace AspNetCoreMVCCRUD.Models
         [Key]
         public int EmployeeId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Full Name")]
         [Column(TypeName = "nvarchar(250)")]
         public string FullName { get; set; }
 
+        [Display(Name = "Code")]
         [Column(TypeName = "varchar(10)")]
         public string Code { get; set; }
 
+        [Display(Name = "Position")]
         [Column(TypeName = "varchar(100)")]
         public string Position { get; set; }
 
+        [Display(Name = "OfficeLocation")]
         [Column(TypeName = "varchar(100)")]
         public string OfficeLocation { get; set; }
     }
